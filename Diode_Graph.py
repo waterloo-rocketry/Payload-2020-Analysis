68181c49a4d8cc44905cf60dffa8712a4b50e1c8
Robert Cai <xiaoyucai2015@gmail.com>
2020-10-03 16:01:49 -0400

    Update Script according to review

--- Diode_Graph.py
+++ Diode_Graph.py
@@ -1,8 +1,12 @@
-import numpy as np
 import csv
-import plotly.express as py
-import pandas as pd
 import datetime
+
+import numpy as np
+import plotly.express as px
+import pandas as pd
+
+microsecondToMinute = 60000000 #Conversion Factor from microsecond to minute 
+microsecondToSecond = 1000000 #Conversion Factor from microsecond to second
 with open("DIODE1.txt", newline='') as csvfile:
     timeArray = []
     dataArray = []
@@ -11,21 +15,21 @@ with open("DIODE1.txt", newline='') as csvfile:
         if(row[0] == "testStarted"):
             print("begin")
             #do something idk what yet
-        else:                
+        else:
             t = int(row[0])
-            min = int(t/59000000)
-            sec = int((t-(59000000*min))/999999)
-            mic = int ((t-(59000000*min)-(999999*sec)))
+            min = int(t/microSecondToMinute)
+            sec = int((t%microSecondToMinute)/microsecondToSecond)
+            mic = t%microsecondToSecond
             readTime = datetime.time(minute=min, second=sec, microsecond=mic)
             readBoard = row[1]
             timeArray.append(readTime)
             dataArray.append(readBoard)
     df = pd.DataFrame(dict(time=timeArray, volt=dataArray))
-    fig = py.scatter(df, x=timeArray, y=dataArray)
+    fig = px.scatter(df, x=timeArray, y=dataArray)
     fig.update_layout(
-    title="Voltage v Time",
-    xaxis_title="Time",
-    yaxis_title="Voltage"
+        title="Voltage v Time",
+        xaxis_title="Minute:Second:Microsecond",
+        yaxis_title="Volt"
     )
     fig.show()
 
