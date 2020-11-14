857ce9b8b368922f9545673510009cc374d2f37c
pcmodeactivate <349348193@gapps.yrdsb.ca>
2020-11-14 03:42:18 -0500

    secant line method preprocessing

--- Preprocess_Data.py
+++ Preprocess_Data.py
@@ -11,10 +11,21 @@ def oddMedian(datacluster):
     datacluster = sorted(datacluster, key = operator.itemgetter(1)) #Sort the data group by y-value
     return datacluster[int(len(datacluster)/2)] #Return the one in the middle
 
+def tangentialSimilarity(datacluster, cutoffslope, filestream): #cutoff is arbitrary
+    sortedcluster = sorted(datacluster, key = operator.itemgetter(1)) #Sort the data group by y-value
+    medianx = float(datacluster[int(len(datacluster)/2)][0]) #Find the corresponding x value of the median
+    mediany = float(datacluster[int(len(datacluster)/2)][1]) #Find the median y value
+    
+    for i in range(len(datacluster)):
+        if (i == int(len(datacluster)/2)):
+            filestream.write(str(int(medianx))+" : "+str(mediany)+"\n") #If the median is iterated, write to file
+        elif (abs((float(datacluster[i][1])-mediany)/(float(datacluster[i][0]) - medianx))>cutoffslope): #Check if the abs value of the slope is greater
+            filestream.write(datacluster[i][0]+":"+datacluster[i][1]+"\n") #If is then write to file
+
 #Enter desired filename here
 filename = "DIODE TEST 2020-10-07"
 
-trimmed = open("Photodiode Test Data/Trimmed_{}.txt".format(filename),"w")
+trimmed = open("Photodiode Test Data/TrimmedV2_{}.txt".format(filename),"w")
 with open("Photodiode Test Data/{}.txt".format(filename), newline='') as csvfile:
     
     datafile = csv.reader(csvfile, delimiter=':')
@@ -32,9 +43,12 @@ with open("Photodiode Test Data/{}.txt".format(filename), newline='') as csvfile
                 datacluster.append((row[0],row[1])) #Append current row to datacluster
             else:
                 datacluster.append((row[0],row[1])) #Append current row to datacluster
-                trimmed.write(str(oddMedian(datacluster)[0])+":"+str(oddMedian(datacluster)[1])+"\n") #Write the median to the trimmed file
+                tangentialSimilarity(datacluster, 0.05, trimmed) #Arbitrary Cutoff Value
+                #trimmed.write(str(oddMedian(datacluster)[0])+":"+str(oddMedian(datacluster)[1])+"\n") #Write the median to the trimmed file
                 datacluster=[] #Clear the cluster
     if (len(datacluster)>0):
-        trimmed.write(str(oddMedian(datacluster)[0])+":"+str(oddMedian(datacluster)[1])+"\n") #Process the remaining points in the cluster
+        tangentialSimilarity(datacluster, 0.05, trimmed)
+        #trimmed.write(str(oddMedian(datacluster)[0])+":"+str(oddMedian(datacluster)[1])+"\n") #Process the remaining points in the cluster
+print("end")
 trimmed.close()
 
