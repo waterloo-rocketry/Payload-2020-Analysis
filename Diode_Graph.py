9beecbc93fc265d24bdf063a547e23ae1054bd5b
Robert Cai <xiaoyucai2015@gmail.com>
2021-03-01 20:12:25 -0500

    diode graph (#1)
    * diode graph
    
    * Update README.md
    
    * Update Script according to review
    
    * fix case issue
    
    * renaming and what not
    
    * Fixed time, and added a bit of functionality for removing base layer of noise & splitting up huge files
    
    * Updated README and created file for test data
    
    * Delete DIODE1.txt
    
    * Delete DIODE3.TXT
    
    * updated readme and requirements
    
    * Addressed Jacobs comments & basically nuked everything
    
    * Fixed script after I made a mess of it. Changed script so it saves graphs in a new graph directory, and added variable for filename.
    
    * Minor Commenting & Directory Fix
    
    * minor fixes for merging pr
    
    * added ws to improve readability
    
    Co-authored-by: pcmodeactivate <349348193@gapps.yrdsb.ca>
    Co-authored-by: Lana Tomlin <lana.motom@gmail.com>
    Co-authored-by: Lana Tomlin <45643942+LanaTomlin@users.noreply.github.com>

---
+++ Diode_Graph.py
@@ -0,0 +1,40 @@
+import csv
+import datetime
+import numpy as np
+import pandas as pd
+import plotly.express as px
+#Enter desired filename here
+filename = "DIODE TEST 2020-10-06"
+
+
+millisecondToHour = 60*60*1000 #Conversion Factor from millisecond to minute 
+millisecondToMinute = 60*1000 #Conversion Factor from millisecond to minute 
+millisecondToSecond = 1000 #Conversion Factor from millisecond to second
+with open(f"Photodiode_Test_Data/{filename}.txt") as csvfile:
+    timeArray = []
+    dataArray = []
+    #count = 0  --- Debug Tool
+    data = csv.reader(csvfile, delimiter=':')
+    #sections = 0   --- Debug Tool
+    for row in data:
+        if row[0] == "testStarted":
+            print("begin")
+        else:
+            t = int(row[0])
+            hour = int(t / millisecondToHour)
+            minute = int(t % millisecondToHour / millisecondToMinute)
+            sec = int((t % millisecondToHour % millisecondToMinute) / millisecondToSecond)
+            mic = t % millisecondToSecond * millisecondToSecond #MillisecondToSecond = MicrosecondToSecond = 1000
+            readTime = datetime.time(hour = hour, minute = minute, second = sec, microsecond = mic)
+            readBoard = row[1]
+            timeArray.append(readTime)
+            dataArray.append(readBoard)
+
+df = pd.DataFrame(dict(time=timeArray, volt=dataArray))
+fig = px.scatter(df, x=timeArray, y=dataArray)
+fig.update_layout(
+    title="Voltage v Time",
+    xaxis_title="Hour:Minute:Second:Microsecond",
+    yaxis_title="Volt"
+)
+fig.write_html("graph/GRAPH-{}.html".format(filename))
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Payload-2020-Analysis
+
+This script in its current state takes in inputs in csv format from text files in the "test_data" directory and generates a graph in the "graph" directory.
+
+Enter/Replace the input file name as the "filename" variable on line 7
+
+Input file should have format of:
+
+testStarted
+time (in microseconds?) : voltage
+time : voltage
+...
+
+
+Due to large file sizes, the program takes a considerable amount of time to run. 
+It is not recommended to use "fig.show()" as that often creates an extremely laggy figure. 
